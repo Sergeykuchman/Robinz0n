5fe29c074a88922005661f49bf5e428b73d06750
Sergey <Sergey.kuchman@gmail.com>
2020-06-27 19:21:53 +0300

    task1

--- java.js
+++ java.js
@@ -1,4 +1,19 @@
 "use strict";
 
-let i=0;
-console.log(i);
+let numberOfFilms=prompt("Сколько фильмов просмотрели","");
+let personaMovieDB={
+    count: 0,
+    movies:{},
+    actors:'',
+    genres:[],
+    privat:false
+};
+
+personaMovieDB["count"]=numberOfFilms;
+
+let movie=prompt("Последний фильм какой Вы посмотрели: ","");
+let raiting=prompt("Оцените: ","");
+
+ personaMovieDB.movies[movie]=raiting;
+
+console.log(personaMovieDB);
